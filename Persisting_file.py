a999357e3f146de6db824067dfe5f5cb11058f90
Yujing Lin <67212190+YujingLin123@users.noreply.github.com>
2024-08-13 21:35:51 +0800

    Update Persisting_file.py

--- Persisting_file.py
+++ Persisting_file.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.externals import joblib
 
-music_data = pd.read_csv('music.csv')
+music_data = pd.read_csv('music.csv')   # control+/ 同时注释多行
 X = music_data.drop(columns=['genre'])
 y = music_data['genre']
 
