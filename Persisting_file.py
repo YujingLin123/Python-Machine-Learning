8ef012702da992b3048cf2e2607e066e095f62d4
Yujing Lin <67212190+YujingLin123@users.noreply.github.com>
2024-08-13 21:38:31 +0800

    Update Persisting_file.py

--- Persisting_file.py
+++ Persisting_file.py
@@ -12,3 +12,21 @@ model.fit(X, y)
 joblib.dump(model, 'music-recommender.joblib')
 
 #predictions = model.predict(X_test)
+
+
+## load the model and run it for making predictions
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.externals import joblib
+
+# music_data = pd.read_csv('music.csv')
+# X = music_data.drop(columns=['genre'])
+# y = music_data['genre']
+
+# model = DecisionTreeClassifier()
+# model.fit(X, y)
+
+model = joblib.load( 'music-recommender.joblib')
+predictions = model.predict([ [21, 1] ])
+predictions
+
